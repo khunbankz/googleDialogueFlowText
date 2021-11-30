d1b0982b31ede0808043350cc94b7cfd9d5f9677
justl <Justlikeheaven.chan@gmail.com>
2021-11-30 11:26:46 +0700

    clean code

--- Googledialogflow/src/main/java/com/example/googledialogflow/Dialogflow.java
+++ Googledialogflow/src/main/java/com/example/googledialogflow/Dialogflow.java
@@ -47,7 +47,6 @@ import java.util.concurrent.TimeUnit;
 public class Dialogflow implements DialogueInputText{
     private  String jsonCredential;
     private  String projectId;
-    //private  String projectIdhaha ="";
     private  String sessionId;
     private SessionsSettings sessionsSettings;
     private String languageCode;
@@ -56,76 +55,14 @@ public class Dialogflow implements DialogueInputText{
     private Config config;
     private Log log;
     private  int length;
-    //String oldmessage ;
     private int maximum;
     private int timeoutsession;
     private String messageInput;
-    //String projectIdhaha = "";
     private String messageOld;
     private long startTimes;
     private long endTimes;
     private long duration;
 
-
-//    public void configId(String projectID,String sessionID,SessionsSettings sessionsSettings){
-////        this.projectId =projectID;
-////        this.sessionId = sessionID;
-//        //this.sessionsSettings =sessionsSettings;
-//        System.out.println("projectID: "+projectID);
-//        System.out.println("sessionID: "+sessionID);
-//        //System.out.println("sessionsSettings: "+sessionsSettings);
-//
-//    }
-//    public void speechs(String input){
-//
-//        List<String> texts = new ArrayList<>();
-//        texts.add(input);
-//        Map<String, QueryResult> queryResults = Maps.newHashMap();
-//        try(SessionsClient sessionsClient = SessionsClient.create(sessionsSettings)){
-//            SessionName session = SessionName.of(projectId, sessionId);
-//            System.out.println("Session Path: " + session.toString());
-//
-//            // Detect intents for each text input
-//            for (String text : texts) {
-//                // Set the text (hello) and language code (en-US) for the query
-//                String message ;
-//                TextInput.Builder textInput =
-//                        TextInput.newBuilder().setText(text).setLanguageCode(languageCode);
-//
-//                // Build the query with the TextInput
-//                QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
-//
-//                // Performs the detect intent request
-//                DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
-//
-//                // Display the query result
-//                QueryResult queryResult = response.getQueryResult();
-//
-//                System.out.println("====================");
-//                System.out.format("Query Text: '%s'\n", queryResult.getQueryText());
-//                System.out.format(
-//                        "Detected Intent: %s (confidence: %f)\n",
-//                        queryResult.getIntent().getDisplayName(), queryResult.getIntentDetectionConfidence());
-//                System.out.format(
-//                        "Fulfillment Text: '%s'\n",
-//                        queryResult.getFulfillmentMessagesCount() > 0
-//                                ? queryResult.getFulfillmentMessages(0).getText()
-//                                : "Triggered Default Fallback Intent");
-//                System.out.println("answer_response: "+queryResult.getFulfillmentText());
-//                message = queryResult.getFulfillmentText();
-//                this.responsemessage = message;
-//
-//                //System.out.println(messa);
-//
-//
-//                queryResults.put(text, queryResult);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        this.listener.OnResult(responsemessage,1.0,"");
-//    }
 
     @Override
     public void open(ResultListener listener) {
@@ -141,10 +78,6 @@ public class Dialogflow implements DialogueInputText{
         GoogleCredentials credentials = null;
         try {
             credentials = GoogleCredentials.fromStream(stream);
-            //credentials.refreshIfExpired();
-
-            //AccessToken token = credentials.getAccessToken();
-            //System.out.println("Access Token"+ token);
             projectId = ((ServiceAccountCredentials)credentials).getProjectId();
             SessionsSettings.Builder settingsBuilder = SessionsSettings.newBuilder();
             settingsBuilder
@@ -160,19 +93,7 @@ public class Dialogflow implements DialogueInputText{
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        GoogleCredentials credentials;
-//        String projectIdhaha = "";
-//        {
-//            try {
-//                credentials = GoogleCredentials.fromStream(new FileInputStream("C:\\Users\\justl\\AndroidStudioProjects\\MyApp2\\app\\src\\main\\res\\raw\\application_default_credentials.json"));
-//                credentials.refreshIfExpired();
-//                projectIdhaha = ((ServiceAccountCredentials)credentials).getProjectId();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
         this.startTimes = System.currentTimeMillis();
-        //config.init(config,log,sessionsSettings);
         length = input.length();
         if (length > maximum){
             log.error("Error:"," Too many characters input ");
@@ -181,8 +102,6 @@ public class Dialogflow implements DialogueInputText{
         }
         List<String> texts = new ArrayList<>();
         texts.add(input);
-        //config.get("hello");
-        //Map<String, QueryResult> queryResults = Maps.newHashMap();
         try(SessionsClient sessionsClient = SessionsClient.create(sessionsSettings)){
             SessionName session = SessionName.of(projectId, sessionId);
             System.out.println("Session Path: " + session.toString());
@@ -230,34 +149,10 @@ public class Dialogflow implements DialogueInputText{
 
 
                 message = queryResult.getFulfillmentText();
-                //message = null;
-                //long endTimes = System.currentTimeMillis();
-                //long duration = endTimes-startTimes;
+
                 this.responsemessage = message;
                 this.log.info(message, String.valueOf(queryResult.getIntentDetectionConfidence()));
-//                if (message == oldmessage){
-//                    log.error("Error","check your connection");
-//                }
-//                oldmessage = message;
 
-                //this.listener = message;
-                //this.responsemessage = message;
-                //JSONParser parser = new JSONParser();
-                //InputStream stream = getClass().getResource("application_default_credentials.json");
-                //URL url = getClass().getResource("application_default_credentials.json");
-                //FileReader reader = new FileReader("C:\\Users\\justl\\AndroidStudioProjects\\MyApp2\\Googledialogflow\\src\\application_default_credentials.json");
-                    //File jsonfile = new File("C:\\Users\\justl\\AndroidStudioProjects\\MyApp2\\app\\src\\application_default_credentials.json");
-                    //Object obj = parser.parse(reader);
-                    //JSONObject jsonObject = (JSONObject) obj;
-                    //String project = (String) jsonObject.get(projectId);
-                    //this.projectIdName=project;
-                    //System.out.println("projectId" + project);
-
-
-                //System.out.println(messa);
-
-
-                //queryResults.put(text, queryResult);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -271,12 +166,6 @@ public class Dialogflow implements DialogueInputText{
         init(config, log, "");
 
     }
-
-
-//    public String getResponsemessage(){
-//        return responsemessage;
-//    }
-
     @Override
     public void close() {
         if (responsemessage == ""){
@@ -293,20 +182,6 @@ public class Dialogflow implements DialogueInputText{
                     }, 5000);
         }
 
-//        try {
-//            TimeUnit.SECONDS.sleep(5);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-
-//            endTimes = System.currentTimeMillis();
-//            duration = endTimes-startTimes;
-            //System.out.println("result time"+duration);
-//            if (responsemessage == ""){
-//                log.error("Error","No response from dialogflow");
-//            }
-
-
         }
 
 
@@ -318,10 +193,8 @@ public class Dialogflow implements DialogueInputText{
         this.languageCode = config.get("language");
         this.maximum = config.getAsInteger("maximum");
         this.timeoutsession = config.getAsInteger("timeout");
-        //this.projectIdhaha=this.config.get("https://raw.githubusercontent.com/Sumethchan/credentials/679cf3d865edca3f6c5a0aeb9e2c9a56974d975a/application_default_credentials.json");
-        //this.projectIdhaha = config.get("Hello");
         this.log=log;
-        //this.sessionsSettings = (SessionsSettings) extended_arg;
+
     }
 
 
